4787ebcc34f032a51e8015150d3eebf2eefe47ed
matthova <matthova87@gmail.com>
2017-03-09 21:42:43 -0800

    added version checking and version form

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
-const simpleGit = require('simple-git')(process.env.PWD);
+const git = require('simple-git')(process.env.PWD);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  simpleGit.revparse(["--tags"], (err, tagShas) => {
-    simpleGit.tags((err, tags) => {
-      console.log('tags', tags, tagShas);
-      res.render('index', {
-        title: 'Express',
-        tags,
+// find all the versions
+// find the shas for each version
+// associate the shas with each version
+//
+// find the current sha
+// determine which version is associated with the current sha
+
+// find all the versions
+  git.tags((err, tags) => {
+// find the shas for each version
+    git.revparse(['--tags'], (err, tagShas) => {
+      tagShas = tagShas.split('\n');
+      tagShas.pop(); // remove excess space
+      if (tags.all.length !== tagShas.length) {
+        throw new Error('Git error. Should be equal number tags and tag SHAs');
+      }
+      const versionList = [];
+// associate the shas with each version
+      for (let i = 0; i < tagShas.length; i++) {
+        versionList.push({
+          version: tags.all[i],
+          sha: tagShas[i],
+        });
+      }
+
+// find the current sha
+      git.revparse(["HEAD"], (err, headSha) => {
+        let version = undefined;
+
+// determine which version is associated with the current sha
+        versionList.forEach(version => {
+          if (version.sha === headSha) {
+            version = version.version
+          }
+        })
+
+        if (!version) {
+          version = headSha;
+          versionList.push({
+            version: 'HEAD',
+            sha: headSha,
+          });
+        }
+
+        res.render('index', {
+          title: 'Express',
+          tags: versionList,
+          version,
+        });
       });
     });
   });
