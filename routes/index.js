5eca6f51a692032a4e2b808a0ca1ae792e204f08
matthova <matthova87@gmail.com>
2017-03-12 11:17:27 -0700

    refactored collecting git tags

--- routes/index.js
+++ routes/index.js
@@ -3,85 +3,82 @@ const router = express.Router();
 
 const git = require('simple-git')(process.env.PWD);
 
-git.fetch(() => {
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
-  // find all the versions
-  // find the shas for each version
-  // associate the shas with each version
-  //
-  // find the current sha
-  // determine which version is associated with the current sha
+function createTagList() {
+  return new Promise((r1, e1) => {
+    git.tags(async (err, tags) => {
+      // find the shas for each version
+      const tagList = [];
 
-  // find all the versions
-    git.tags((err, tags) => {
-  // find the shas for each version
-      git.revparse(['--tags'], (err, tagShas) => {
-        tagShas = tagShas.split('\n');
-        tagShas.pop(); // remove excess space
-        if (tags.all.length !== tagShas.length) {
-          throw new Error('Git error. Should be equal number tags and tag SHAs');
-        }
-        const versionList = [];
-  // associate the shas with each version
-        for (let i = 0; i < tagShas.length; i++) {
-          versionList.push({
-            version: tags.all[i],
-            sha: tagShas[i],
-          });
-        }
-
-  // find the current sha
-        git.revparse(["HEAD"], (err, headSha) => {
-          let version = undefined;
-
-  // determine which version is associated with the current sha
-          versionList.forEach(version => {
-            if (version.sha === headSha) {
-              version = version.version
-            }
-          })
-
-          if (!version) {
-            version = headSha;
-            versionList.push({
-              version: 'HEAD',
-              sha: headSha.split('\n')[0],
-            });
-          }
-
-          res.render('index', {
-            title: 'Express',
-            tags: versionList,
-            version,
+      const tagArray = tags.all.map(tag => {
+        return new Promise((r2, e2) => {
+          git.revparse(['--verify', `${tag}^{commit}`], (err, result) => {
+            const sha = result.split('\n')[0];
+            tagList.push({ tag, sha });
+            r2();
           });
         });
+      });
+
+      await Promise.all(tagArray);
+      r1(tagList);
+    });
+  });
+}
+
+git.fetch(() => {
+  /* GET home page. */
+  router.get('/', async function(req, res, next) {
+  // find all the versions
+    const tagList = await createTagList();
+
+    // find the current sha
+    git.revparse(["HEAD"], (err, headSha) => {
+      headSha = headSha.split('\n')[0];
+      let version = undefined;
+
+    // determine which version is associated with the current sha
+      tagList.forEach(tagObject => {
+        if (tagObject.sha === headSha) {
+          version = tagObject.tag;
+        }
+      });
+
+      // If on a non-tagged commit, then just call it "HEAD"
+      if (!version) {
+        version = headSha;
+        tagList.push({
+          tag: 'HEAD',
+          sha: headSha.split('\n')[0],
+        });
+      }
+
+      res.render('index', {
+        title: 'Express',
+        tags: tagList,
+        version,
       });
     });
   });
 
-  router.post('/update', function(req, res, next) {
-    // load all available shas
-    git.revparse(['--tags'], (err, tagShas) => {
-      tagShas = tagShas.split('\n');
-      tagShas.pop();
-      // check the sha received is a match
-      const match = tagShas.find(sha => {
-        return sha === req.body.version;
-      });
+  router.post('/update', async function(req, res, next) {
+    const tagList = await createTagList();
 
-      // if it is, then check out the sha
-      if (match) {
-        git.checkout(match, (error, response) => {
-          if (error) {
-            return res.json({ error });
-          }
-        return res.json({ hello: 'new version'});
-        });
-      } else {
-        return res.send('nope');
-      }
+    // check the sha received is a match
+    const match = tagList.find(tag => {
+      return tag.sha === req.body.version;
     });
+
+    // if it is, then check out the sha
+    if (match) {
+      git.checkout(match.sha, (error, response) => {
+        if (error) {
+          return res.json({ error });
+        }
+        return res.redirect('/');
+      });
+    } else {
+      return res.send('nope');
+    }
   });
 });
 
